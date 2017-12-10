7f253a37bf62dc0ad29e63a13e836e2615a3c9a4
mauriciogodinez <mauricio.godinez.contractor@bbva.com>
2017-12-10 05:26:29 -0600

    add: formato de codigo, agregan interfaces e inicio de modelo vista presentador

--- app/src/main/java/com/example/mauriciogodinez/migraciontoken/MainActivity.java
+++ app/src/main/java/com/example/mauriciogodinez/migraciontoken/MainActivity.java
@@ -1,32 +1,35 @@
 package com.example.mauriciogodinez.migraciontoken;
 
-import android.content.Context;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 
+import com.example.mauriciogodinez.migraciontoken.ui.TokenDialogFragment;
+
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton mToken;
+
+    DialogFragment dFragmentToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppCompatButton boton = (AppCompatButton) findViewById(R.id.boton_actividad);
+        mToken = findViewById(R.id.boton_actividad);
 
-        boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                lanza_alert();
-            }
-        });
+        mToken.setOnClickListener(fragmentTokenListener);
     }
 
-    private void lanza_alert() {
-        DialogFragment dialog = new TokenDialogFragment();
-        dialog.setCancelable(false);
-        dialog.show(getSupportFragmentManager(), "TokenDialogFragment");
-    }
+    View.OnClickListener fragmentTokenListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            dFragmentToken = new TokenDialogFragment();
+
+            dFragmentToken.setCancelable(false);
+            dFragmentToken.show(getSupportFragmentManager(), "TokenDialogFragment");
+        }
+    };
 }
